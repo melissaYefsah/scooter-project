d8be9a7781518d606fb780b569860bc2788fc28c
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-10-30 16:31:56 -0400

    Added Scooter class and it's test

--- __tests__/Scooter.js
+++ __tests__/Scooter.js
@@ -2,28 +2,37 @@ const { describe, expect, it } = require("@jest/globals");
 const Scooter = require("../classes/Scooter.js");
 
 describe("scooter.rent(user)", () => {
-  it.skip("checks a scooter out to a user", () => {
-    // Arrange
-    // Act
-    // Assert
+  const scooter = new Scooter('station1');
+
+  it("checks a scooter out to a user", () => {
+    scooter.charge = 30;
+    scooter.isBroken = false;
+    expect(scooter.rent('mel')).toEqual('mel');
   });
 
-  it.skip("throws an error if battery dead or scooter broken", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if battery dead or scooter broken", () => {
+    scooter.charge = 10;
+    scooter.isBroken = false;
+    expect(() => scooter.rent('mel')).toThrow('scooter needs to charge');
+    
+    scooter.charge = 30;
+    scooter.isBroken = true;
+    expect(() => scooter.rent('mel')).toThrow('scooter needs repair');
   });
 });
 
 describe("scooter.dock(station)", () => {
-  it.skip("returns a scooter to a station", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("returns a scooter to a station", () => {
+    const scooter = new Scooter('station1');
+    expect(scooter.dock('station2')).toBeUndefined(); // dock method returns nothing
+    expect(scooter.station).toBe('station2');
+    expect(scooter.user).toBeNull();
   });
 });
 
-describe("scooter.charge()", () => {
+
+
+/*describe("scooter.charge()", () => {
   it.skip("charges a scooter", () => {
     // Arrange
     // Act
@@ -37,4 +46,4 @@ describe("scooter.repair()", () => {
     // Act
     // Assert
   });
-});
+});*/
--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,30 @@
 class Scooter {
   // Scooter code here
+  static nextSerial=1;
+  constructor(station) {
+    this.station=station;
+    this.user=null;
+    this.serial = Scooter.nextSerial++;
+    this.charge=100;
+    this.isBroken=false;
+    
+  }
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      this.station = null;
+      this.user = user;//Return the user that rent the scooter
+      return user; 
+    } else if (this.charge <= 20) {
+      throw new Error('scooter needs to charge');
+    } else if (this.isBroken) {
+      throw new Error('scooter needs repair');
+    }
+  }
+  dock(station){
+    this.station = station ;
+    this.user = null;
+  }
+
 }
 
 module.exports = Scooter;
