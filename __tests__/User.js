ff0259c9bba515acf46c6648bce5e1b25977aaba
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-10-31 09:27:55 -0400

    Added User class and it's test class

--- __tests__/User.js
+++ __tests__/User.js
@@ -2,23 +2,24 @@ const { describe, expect, it } = require("@jest/globals");
 const User = require("../classes/User.js");
 
 describe("user.login(password)", () => {
-  it.skip("logs a user in if the password is correct", () => {
-    // Arrange
-    // Act
-    // Assert
+  const user = new User('mel_yef', '123', 24);
+
+  it("logs a user in if the password is correct", () => {
+    user.login('123'); // Call the login method
+    expect(user.LoggedIn).toBe(true); // Check if LoggedIn is true
   });
 
-  it.skip("throws an error if the password is incorrect", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if the password is incorrect", () => {
+    expect(() => user.login('222')).toThrow("Incorrect Password");
   });
 });
 
 describe("user.logout()", () => {
-  it.skip("logs a user out", () => {
-    // Arrange
-    // Act
-    // Assert
+  const user = new User('mel_yef', '123', 24);
+
+  it("logs a user out", () => {
+    user.login('123'); // Log in first
+    user.logout(); // Log out
+    expect(user.LoggedIn).toBe(false); // Check if LoggedIn is false
   });
 });
--- classes/User.js
+++ classes/User.js
@@ -1,5 +1,22 @@
 class User {
   // User code here
+  constructor(username,password,age){
+    this.username = username ;
+    this.password=password;
+    this.age=age;
+    this.LoggedIn = false ;
+  }
+  login(password){
+    if (this.password === password){
+      this.LoggedIn=true;
+      return true;
+    }else {
+      throw new Error('Incorrect Password');
+    }
+  }
+  logout(){
+    this.LoggedIn = false ;
+  }
 }
 
 module.exports = User;
